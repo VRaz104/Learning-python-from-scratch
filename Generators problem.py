861bd92caa74bf96d0fd5966c8a7fd4cc3d9fbc0
VRaz104 <razvanvitan73@gmail.com>
2025-11-19 23:53:08 +0200

    Generators problem
    Simple problem using generators to understand a bit how they work

--- Generators problem.py
+++ Generators problem.py
@@ -1,12 +1,14 @@
-#A simple problem to understand generators
-#Write a generator function called prime_numbers(limit) that yields all prime numbers up to a given limit. Then, use it to print all primes less than 50.
+#A simple problem to understand generators 
+##Write a generator function called prime_numbers(limit) that yields all prime numbers up to a given limit. Then, use it to print all primes less than 50.
 def prime_numbers(limit):
-    for i in range(2, limit+1):
-        for j in range(2, limit/2+1):
-            if i%j==0:
+    for i in range(2, limit + 1):
+        is_prime = True
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                is_prime = False
                 break
-            else:
-                yield i
+        if is_prime:
+            yield i
 limit = int(input("What is the limit? "))
 numbers = prime_numbers(limit)
 for n in numbers:
